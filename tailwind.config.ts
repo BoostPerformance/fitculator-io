3ddc37ed37b97d7a10cd11f855fde500dde6ef14
Eunji <danceunji@gmail.com>
2024-07-26 04:29:58 +0200

    프로젝트 초기 세팅

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,48 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
       },
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Pretendard', 'sans-serif'],
+      },
+      colors: {
+        blue: {
+          DEFAULT: '#E4EFF5',
+        },
+        gray: {
+          DEFAULT: '#F2F2F2',
+          1: '#625C5C',
+        },
+      },
+    },
+    fontSize: {
+      '1.75-500': ['1.75rem', { fontWeight: 500 }],
+      '1.75-700': ['1.75rem', { fontWeight: 700 }],
+
+      '2-700': ['2rem', { fontWeight: 700 }],
+
+      '2.5-700': ['2.5rem', { fontWeight: 700 }],
+      '2.5-900': ['2.5rem', { fontWeight: 900 }],
+
+      '1.6-700': ['1.6rem', { fontWeight: 700 }],
+    },
+    screens: {
+      mac: { max: '2300px' },
+      md: { max: '1199px' },
+      sm: { max: '743px' },
     },
   },
   plugins: [],
