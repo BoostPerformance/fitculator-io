858991bc7c00275afda4bf71d28b787fa0c6224e
Eunji <danceunji@gmail.com>
2024-07-29 01:16:35 +0200

    applySection 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,43 +4,12 @@ import Section from '@/components/section';
 import Reviews from '@/components/reviews';
 import Product from '@/components/products';
 import AppleWatchSection from '@/components/appleWatchSection';
+import ApplySection from '@/components/applySection';
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center">
-      <section className=" relative mb-10">
-        <div className="absolute flex flex-col items-center justify-center w-full h-[30rem] mt-[20rem] md:mt-1">
-          <div>
-            <Section
-              title={
-                <>
-                  <p className="text-3-700">
-                    정확한 운동량을 알려주는 <br /> 나만의 피트니스 가이드,{' '}
-                    <br />
-                    핏큘레이터
-                  </p>
-                </>
-              }
-              description={<>핏큘레이터와 함께 꾸준한 운동을 생활화 하세요.</>}
-            />
-
-            <Button version="ver2">핏큘레이터 신청하기</Button>
-          </div>
-        </div>
-        <div className="w-full bg-gradient-to-t">
-          <Image
-            src="/images/landing-running.png"
-            alt="woman running"
-            width={2300}
-            height={10}
-            objectFit="cover"
-          />
-          <div
-            className="absolute inset-0 bottom-0 left-0 right-0 h-2/5 bg-gradient-to-t from-white to-transparent"
-            style={{ top: '60%' }}
-          />
-        </div>
-      </section>
+      <ApplySection />
 
       <AppleWatchSection />
 
