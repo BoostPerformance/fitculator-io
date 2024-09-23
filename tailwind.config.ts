931812a872c758886e41823bb0c1c36c2a3d408d
Sung Eunji <116065415+sung-eunji@users.noreply.github.com>
2024-09-23 13:06:56 +0200

    리뷰 카드 애니메이션 수정 (#21)
    * 리뷰카드 애니메이션 수정
    
    * 애니메이션 끊김 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,16 +13,16 @@ const config: Config = {
     },
     extend: {
       animation: {
-        marquee: 'marquee 120s linear infinite',
-        marquee2: 'marquee2 120s linear infinite',
+        marquee: 'marquee 130s linear infinite',
+        marquee2: 'marquee2 130s linear infinite',
       },
       keyframes: {
         marquee: {
           '0%': { transform: 'translateX(0%)' },
-          '100%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(-60%)' },
         },
         marquee2: {
-          '0%': { transform: 'translateX(-100%)' },
+          '0%': { transform: 'translateX(-60%)' },
           '100%': { transform: 'translateX(0%)' },
         },
       },
@@ -50,7 +50,6 @@ const config: Config = {
           9: '#E0E0E0',
           10: '#BDBDBD',
           11: '#A1A1A1',
-          12: '#767676',
         },
         red: {
           DEFAULT: '#DD204E',
@@ -80,7 +79,6 @@ const config: Config = {
       '1.875-500': ['1.875rem', { fontWeight: 500 }],
       '2-700': ['2rem', { fontWeight: 700 }],
       '2-900': ['2rem', { fontWeight: 900 }],
-      '2.25-700': ['2.25rem', { fontWeight: 700 }],
       '2.5-700': ['2.5rem', { fontWeight: 700 }],
       '2.5-900': ['2.5rem', { fontWeight: 900 }],
       '3-700': ['3rem', { fontWeight: 700 }],
@@ -88,7 +86,7 @@ const config: Config = {
       '4.25-500': ['4.25rem', { fontWeight: 500 }],
     },
     screens: {
-      md: { max: '1073px' },
+      md: { max: '1024px' },
       sm: { max: '768px' },
     },
   },
