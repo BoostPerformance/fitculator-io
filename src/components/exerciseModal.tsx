305298b4787e4210166642ddc327741aa5e5c48c
Eunji <danceunji@gmail.com>
2024-09-30 06:37:08 +0200

    목표운동량 모달 UI수정

--- src/components/exerciseModal.tsx
+++ src/components/exerciseModal.tsx
@@ -6,7 +6,7 @@ interface ExerciseModalProps {
 
 export default function ExerciseModal({ handleModal }: ExerciseModalProps) {
   return (
-    <div className="fixed inset-0 z-50 flex justify-center items-center bg-black bg-opacity-50 overflow-y-auto">
+    <div className="sticky inset-0 z-50 flex justify-center items-center bg-black bg-opacity-50 overflow-y-auto">
       <div className="bg-white sm:w-[22rem] rounded-[0.625rem] h-[53rem]">
         <ExerciseGuideline />
         <div className="flex justify-end">
