a2f295b9216c1c175c240990362cd8a6131cd181
Eunji <danceunji@gmail.com>
2024-10-01 13:36:19 +0200

    footer 반응형 수정

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,11 +3,11 @@ import Nav from './nav';
 
 const Footer = () => {
   return (
-    <footer className="pt-[4.7rem] pb-[6.8rem] px-[10rem] bg-gray-2 sm:pl-[1.5rem] sm:px-[1.87rem] sm:pb-[6.5rem] sm:pt-[2.5rem] md:px-0 relative z-0">
+    <footer className="pt-[4.7rem] pb-[6.8rem] px-[10rem] bg-gray-2 md:px-[2rem] sm:pl-[1.5rem] sm:px-[0.8rem] sm:pb-[6.5rem] sm:pt-[2.5rem] relative z-0">
       <Nav navMenu smWidth="sm:w-[10.6rem]" />
 
-      <div className="flex flex-row px-[3rem] md:px-[2.5rem] justify-between sm:flex-col-reverse gap-[11rem] mt-[4.5rem] sm:gap-[1rem] sm:items-start sm:m-0 sm:px-[1rem] ">
-        <p className="text-gray-1 md:text-1.125-500 sm:text-1.125-500 sm:items-left sm:my-[1rem]">
+      <div className="flex flex-row px-[3rem] md:px-[2.5rem] justify-between sm:flex-col-reverse gap-[11rem] md:gap-[1rem] mt-[4.5rem] sm:gap-[1rem] sm:items-start sm:m-0 sm:px-[1rem] ">
+        <p className="text-gray-1 md:text-1.125-500 sm:text-1-500 sm:items-left sm:my-[1rem]">
           Fitculator (핏큘레이터) <br />
           사업자등록번호: 262-67-00523 | 대표:류현지 <br />
           통신판매신고: 2023-서울서대문-0576 <br />
