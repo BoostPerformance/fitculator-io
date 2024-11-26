79ab2c6f34995908310faa0d27ce75d251beb391
heejour <cuteprobe@gmail.com>
2024-11-26 16:10:21 +0900

    fix: temporarily hardcode subscription ID and restrict dropdown to single selection

---
+++ src/components/batchesDropdown.tsx
@@ -0,0 +1,22 @@
+import DropdownMenu from '@/components/dropdown';
+import { DropdownOption } from '@/types/types';
+
+export default function BatchesDropdown({
+  rowNO,
+  onChange,
+
+  title,
+}: {
+  rowNO: number;
+  onChange: (item: DropdownOption) => void;
+
+  title: string;
+}) {
+  const Batches: DropdownOption[] = [
+    { id: 1, option: `${rowNO}기` },
+    // { id: 2, option: `${rowNO + 1}기` },
+    // { id: 3, option: `${rowNO + 2}기` },
+  ];
+
+  return <DropdownMenu data={Batches} onChange={onChange} title={title} />;
+}
