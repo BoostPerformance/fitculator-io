e8ec2f51640b341c57e40ebbb6235f7a55f63149
Eunji <danceunji@gmail.com>
2025-05-23 15:16:08 +0200

    fix: optimize loading page

--- src/components/landing-sections/registerSection.tsx
+++ src/components/landing-sections/registerSection.tsx
@@ -5,39 +5,55 @@ import Link from 'next/link';
 
 export default function RegisterSection() {
   return (
-    <section className="relative">
-      <div className="absolute flex items-center justify-center w-full h-[30rem] mt-[8rem] md:h-[38rem] md:mt-[5rem] md:ml-[4rem] sm:top-[1rem] sm:ml-auto z-10 md:items-start md:justify-start sm:items-start sm:justify-start">
-        <div className="flex flex-col gap-[2.5rem]">
+    <section className="relative w-full h-[38rem] sm:h-[28rem]">
+      {/* 이미지 배경 */}
+      <div className="absolute inset-0 w-full h-full">
+        <Image
+          src="/images/landing-running.webp"
+          alt="woman running"
+          fill
+          priority
+          sizes="100vw"
+          className="object-cover sm:object-[70%_center]"
+        />
+        <div
+          className="absolute inset-0 w-[55%]"
+          style={{
+            background:
+              'linear-gradient(270deg, rgba(219, 226, 228, 0.00) 0%, rgba(219, 226, 228, 0.90) 16.32%)',
+          }}
+        />
+        <div className="absolute inset-0 h-[40%] sm:h-[69%] bg-gradient-to-t from-white via-white to-transparent top-[66%] sm:top-[50%] md:top-[60%]" />
+      </div>
+
+      {/* 텍스트 오버레이 */}
+      <div className="absolute top-1/2 left-1/4 transform -translate-y-1/2 -translate-x-[10rem] px-[2rem] z-10 sm:left-[12rem] sm:px-0 ">
+        <div className="flex flex-col gap-[2.5rem] max-w-[600px]">
           <div>
-            <div className="text-3-700 sm:text-left sm:px-[2rem] sm:pb-[0.3rem] sm:text-1.25-700 leading-tight">
-              <div className="sm:pb-[0.7rem]">
-                <span>
-                  무조건 운동하게 만드는
-                  <br />
-                </span>
-
-                <span>
-                  핏큘레이터
-                  <br />
-                </span>
-              </div>
-            </div>
-
-            <p className="sm:px-[2rem] sm:text-left sm:text-0.875-700 text-1.75-500 text-gray-12 sm:pt-[0.6rem]">
-              자기관리의 시작.&nbsp;
+            <h2 className="text-3-700 sm:text-1.25-700 leading-tight sm:pb-[0.7rem]">
+              <span>
+                무조건 운동하게 만드는
+                <br />
+              </span>
+              <span>
+                핏큘레이터
+                <br />
+              </span>
+            </h2>
+            <p className="text-1.75-500 sm:text-0.875-700 text-gray-12 sm:pt-[0.6rem]">
+              자기관리의 시작.
               <br />
-              나에게 필요한 운동량을 매주 채워보세요.
+              나에게 필요한 운동량을 매주
+              <br className="lg:hidden md:hidden sm:inline" />
+              채워보세요.
             </p>
           </div>
 
+          {/* 버튼 */}
           <Link href="#product-section" className="sm:hidden">
             <Button text="시작하기" size="md" variant="default" />
           </Link>
-
-          <Link
-            href="#product-section"
-            className="hidden sm:inline sm:pl-[2rem]"
-          >
+          <Link href="#product-section" className="hidden sm:inline">
             <Button
               text="시작하기"
               size="xs"
@@ -46,26 +62,6 @@ export default function RegisterSection() {
             />
           </Link>
         </div>
-      </div>
-      <div className="relative md:overflow-hidden sm:overflow-hidden md:h-auto sm:h-[28rem]">
-        <Image
-          src="/images/landing-running.webp"
-          alt="woman running"
-          width={2600}
-          height={600} // 대신 layout이 자동 맞춰짐
-          sizes="(max-width: 768px) 100vw, 50vw" // 적절한 크기 선택
-          priority
-          className="object-cover sm:top-[10rem] sm:scale-200 md:scale-150 sm:transform sm:translate-x-[-6rem] sm:translate-y-[6rem] md:translate-x-[-6rem] md:translate-y-[4rem]"
-        />
-
-        <div
-          className="absolute inset-0 w-[55%]"
-          style={{
-            background:
-              'linear-gradient(270deg, rgba(219, 226, 228, 0.00) 0%, rgba(219, 226, 228, 0.90) 16.32%)',
-          }}
-        />
-        <div className="absolute inset-0 h-[40%] sm:h-[69%] bg-gradient-to-t from-white  via-white to-transparent top-[66%] sm:top-[50%] md:top-[60%]" />
       </div>
     </section>
   );
